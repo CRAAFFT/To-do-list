0ebe1f5e97665cc23f17104f879f8f07c2f46243
CRAAFFT <rifaasiraajuddin.123@gmail.com>
2024-07-14 14:12:39 +0700

    Setup Project

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+class Config():
+    SECRET_KEY = 'dsjakhdiadgweabdjacbuekvcbajsia'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/to_do_list'
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    JWT_SECRET_KEY = 'sadkhadgadja vdasudktguywjgasbcdasvduys'
+    
